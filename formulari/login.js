5d6e34c02d810d4e0df0942222e046310d7c1eeb
DavidMorales121 <david2008moralesrosales@gmail.com>
2026-05-28 08:54:43 +0200

    CSS changes

--- formulari/login.js
+++ formulari/login.js
@@ -8,7 +8,6 @@ const togglePassword = document.querySelector('.toggle-password');
 const passwordInput = document.querySelector('input[name="password"]');
 
 togglePassword.addEventListener('click', () => {
-
     if (passwordInput.type == 'password') {
         passwordInput.type = 'text';
         togglePassword.textContent = '👀';
@@ -16,15 +15,83 @@ togglePassword.addEventListener('click', () => {
         passwordInput.type = 'password';
         togglePassword.textContent = '🙈';
     }
+});
+
+
+
+loginForm.addEventListenerTest('submit', (event) => {
+    event.preventDefault(); 
+
+    
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+    const newDiv = document.createElement("div");
+
+    if (email == 'admin@gmail.com' && password == '1234') {
+        newDiv.textContent= mensajeExito
+
+        setTimeout(() => {
+            newDiv.classList.add('hidden');
+        }, 2000);
+
+    } else {
+        newDiv.textContent= mensajeError
+        setTimeout(() => {
+            newDiv.classList.add('hidden');
+        }, 2000);
+
+    }
+    document.body.appendChild(nuevoDiv);
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault(); 
-    
-    mensajeExito.classList.remove('hidden'); 
 
-    setTimeout(() => {
-        mensajeExito.classList.add('hidden');
-    }, 2000);
+    
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    
+    mensajeExito.classList.add('hidden');
+    mensajeError.classList.add('hidden');
+
+    
+    if (email == 'admin@gmail.com' && password == '1234') {
+        mensajeError.classList.remove('hidden');
+        
+        setTimeout(() => {
+            mensajeExito.classList.add('hidden');
+        }, 2000);
+
+    } else {
+        mensajeExito.classList.remove('hidden');
+
+        setTimeout(() => {
+            mensajeError.classList.add('hidden');
+        }, 2000);
+    }
 });
+
